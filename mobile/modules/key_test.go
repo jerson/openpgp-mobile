3771a4b521b40201c97f402d4a047c5f13fbb2f7
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2019-07-09 11:26:52 -0500

    added more methods for sign, verify and encrypt

--- mobile/modules/key_test.go
+++ mobile/modules/key_test.go
@@ -1,13 +1,11 @@
-package openpgp
+package modules
 
 import (
-	"fmt"
+	"errors"
 	"testing"
 )
 
-func TestDecode(t *testing.T) {
-
-	message := `-----BEGIN PGP MESSAGE-----
+var message = `-----BEGIN PGP MESSAGE-----
 Version: BCPG v1.60
 
 hQEMA8lrFZvFE3EOAQgAwkPZb4dgFnSOV9+nmeQScBZ4dS6Gpn16URZyKcReip16
@@ -33,7 +31,7 @@ hntdKQW3PGGKya+6rA==
 =Hz7z
 -----END PGP MESSAGE-----`
 
-	privateKey :=  `-----BEGIN PGP PRIVATE KEY BLOCK-----
+var privateKey = `-----BEGIN PGP PRIVATE KEY BLOCK-----
 
 lQPGBF0Tpe0BCADm+ja4vMKuodkQEhLm/092M/6gt4TaKwzv8QcA53/FrM3g8wab
 oksgIN1T+CscKXOY9GeJ530sBZgu9kTsMeQW3Uf76smo2CV1W7kSTMU+XE5ExGET
@@ -94,11 +92,124 @@ dJmM+fNpXSDjKMpeWxlgNLw9zIVJ+GjUVfsgew/ALE9lwSpxFRrqpDonlf0H83T0
 =yXPi
 -----END PGP PRIVATE KEY BLOCK-----`
 
+var publicKey = `-----BEGIN PGP PUBLIC KEY BLOCK-----
 
+mQENBF0Tpe0BCADm+ja4vMKuodkQEhLm/092M/6gt4TaKwzv8QcA53/FrM3g8wab
+oksgIN1T+CscKXOY9GeJ530sBZgu9kTsMeQW3Uf76smo2CV1W7kSTMU+XE5ExGET
++LhkKeHpXoLfZNegKuzH1Qo7DGDOByVrnijbZIukonGwvC/MTduxb69ub+YRhWJc
+fCjpMhm4WsC4h0Oz3ir5kx6NsN7XGVT5ghR9OEV6jlFmJ1nYYQxMlBGiATN5f2VR
+Y/T9QBLOEsLNvK4OqviLVvgPTQZBePoCeL73CxTbqVhcamvFRVicivl8AphDLATn
+1HlIjsTtbH5I/STb8UUdL+4ziRzsQukEw++xABEBAAG0WFRlc3QgQ2xpZW50IFBy
+ZXBhZ28gKExsYXZlIGRlIGNsaWVudGUgdXRpbGl6YWRhIGVuIHRlc3QgYXV0b21h
+dGl6YWRvcykgPGNsaWVudEB0ZXN0LmNvbT6JAU4EEwEIADgWIQSM7S4gtzn4Xwz9
+EH+B0DnjYl2BcAUCXROl7QIbAwULCQgHAgYVCgkICwIEFgIDAQIeAQIXgAAKCRCB
+0DnjYl2BcI1HB/94YttYwwEDYeaD51gX9QamH2DsayAqPsKyEHmgsJdJcCK27sPb
+EjrI3g8sKze2aG6hPgY+FjLp/sCXWayt8N1wN7U7gnFCu2cszykyTKMwTuZs06w5
+LEWWWZG3WcbVU4EdNVN0sCYr1OfgQiI9CT7/H5HZM12m5u4Qgw+9CFyMBb9sPjMw
+KBMHnQLkp/J0Fmiy5KTdZL+o28lfttAOJDg+ZTChs0LWnvZCrLKquIkX7ugI85iZ
+leehaFWX5Bx9w9D06uERbFlWxlonR94aA9oX9rjXcaNZeDRSpHqh5JlTMSrXXT1k
+LVNLZieBhA033eNaPwMFuKyAlnVBV9d/Qz7guQENBF0Tpe0BCAD3L3Wizb8XHQAb
+UXPqUe4Uc3YpRCXr/1wkIe7atDIP0Y74uxeklHMPFhxO+8pBL/bDrh+4ojdjDW8R
+uE3tgSTDp5Jx4YG32dHwpoTZ1n9N8Fc58MY3zju3n2J3N86vNkST/qDzKEf7SUfi
+u+KE0x3+U0QUostL9iVJXa4zg+te0yTqCt8IzGbQ6g6qCNxbbb9bZi+xDdl8z3f+
++oYIMhakixuwgDsWMblbLH7yE0gkMzEP5P5C1MsR7Ty1rtKL6RPsPvqbCXjiqI2y
+iRrwAFEX1zQhqZrFRyVkzM1uQRGqFV9IDDowK44kxezumS7JLFgrZzrQFY0oOhpz
+oNrWxkalABEBAAGJATYEGAEIACAWIQSM7S4gtzn4Xwz9EH+B0DnjYl2BcAUCXROl
+7QIbDAAKCRCB0DnjYl2BcHlcB/45ISlU8Fjp0JewpZV5gLj2aExDwzJHhjZioay0
+S+FS7jSu3Q7fmh1IDQdk6k0++v31YhMJt/96W4coH3hBBMuU+nCM4Ed6g9TAlhSQ
+RQdxDY2bYSHnYP4wLZ1JMB3SL9vEsfUS+M+NYS6VxkaVo4opwM6AMBxoFfQj+41F
+s5OVkk68O6f4Rd2HInd0DnGQJvTFF1iDTRvPbtmU7hE088yxpGkHudeLtnasqXSZ
+jPnzaV0g4yjKXlsZYDS8PcyFSfho1FX7IHsPwCxPZcEqcRUa6qQ6J5X9B/N09N0i
+D4m65Neoc7DBEdvzgK9IUMpwG5N0t+0pfWLhs8AZdMxE7RbP
+=kbtq
+-----END PGP PUBLIC KEY BLOCK-----`
 
-	openpgp := NewOpenPGP()
-	output := openpgp.Decode(message,privateKey,"test")
+var signed = `-----BEGIN PGP SIGNATURE-----
 
-	fmt.Print("output:")
-	fmt.Print(output)
+wsBcBAABCAAQBQJdJLrXCRCB0DnjYl2BcAAAk8UIADTn6wF4KwOLZ9HV26QhWoqf
+NMf815VILfdk2wzBSlTg5kvHvhRF5p0dxmUiJKHPNGhyMGbecnmMC09EBb5lvh3g
+b9BbZxUfEgDR6JVBtJAXb+l1nilEvEcxXTn87ccuZBsZuKD4A+6Dhs8VJ4faTv7M
+w2QWcxIUjEVjFWcV1vbhsfLPm/UA2rkvKFkuORmHz7aG1FpEIRrVuouusqqdhal0
+AQfFb3JgS9WTqVd1BLGItoeVHS9gTloJrf1rOON6mq3G0TurBtcK86EBeETbg4Yw
+s9nAZmdI3qERI21Uz/AXHKtG/0vKdRep1mOaVOjR4tIVFIYS1NfBkmxUu0yT68w=
+=Tua5
+-----END PGP SIGNATURE-----`
+
+var passphrase = "test"
+var inputMessage = "hola mundo"
+
+func TestOpenPGPkEY_Decrypt(t *testing.T) {
+
+	openPGP := NewOpenPGPKey()
+	output, err := openPGP.Decrypt(message, privateKey, passphrase)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("output:", output)
+}
+
+func TestOpenPGPKey_Encrypt(t *testing.T) {
+
+	openPGP := NewOpenPGPKey()
+	output, err := openPGP.Encrypt(inputMessage, publicKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("output:", output)
+}
+
+func TestOpenPGPKey_Complete(t *testing.T) {
+
+	openPGP := NewOpenPGPKey()
+	input, err := openPGP.Encrypt(inputMessage, publicKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	output, err := openPGP.Decrypt(input, privateKey, passphrase)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("output:", output)
+
+	if output != inputMessage {
+		t.Fatal(errors.New("fail"))
+	}
+}
+
+func TestOpenPGPKey_Sign(t *testing.T) {
+
+	openPGP := NewOpenPGPKey()
+	output, err := openPGP.Sign(inputMessage, publicKey, privateKey, passphrase)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("output:", output)
+}
+
+func TestOpenPGPKey_Verify(t *testing.T) {
+
+	openPGP := NewOpenPGPKey()
+	output, err := openPGP.Verify(signed, inputMessage, publicKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("output:", output)
+}
+
+func TestOpenPGPKey_VerifyAndSign(t *testing.T) {
+
+	openPGP := NewOpenPGPKey()
+	input, err := openPGP.Sign(inputMessage, publicKey, privateKey, passphrase)
+	if err != nil {
+		t.Fatal(err)
+	}
+	output, err := openPGP.Verify(input, inputMessage, publicKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("output:", output)
 }
--- mobile/openpgp.go
+++ mobile/openpgp.go
@@ -1,60 +1,13 @@
 package openpgp
 
-import (
-	"fmt"
-	"golang.org/x/crypto/openpgp"
-	"golang.org/x/crypto/openpgp/armor"
-	"io/ioutil"
-	"log"
-	"strings"
-)
+import "github.com/jerson/openpgp-mobile/mobile/modules"
 
 type OpenPGP struct {
+	Key *modules.OpenPGPKey
 }
 
 func NewOpenPGP() *OpenPGP {
-	return &OpenPGP{}
-}
-
-func (o *OpenPGP) Decode(message, privateKey, passphrase string) string {
-
-	var entity *openpgp.Entity
-	var entityList openpgp.EntityList
-
-	keyringFileBuffer := strings.NewReader(privateKey)
-	entityList, err := openpgp.ReadArmoredKeyRing(keyringFileBuffer)
-	if err != nil {
-		return "error reading ring"
+	return &OpenPGP{
+		Key: modules.NewOpenPGPKey(),
 	}
-	entity = entityList[0]
-
-	passphraseByte := []byte(passphrase)
-	log.Println("Decrypting private key using passphrase")
-	_ = entity.PrivateKey.Decrypt(passphraseByte)
-	for _, subKey := range entity.Subkeys {
-		_ = subKey.PrivateKey.Decrypt(passphraseByte)
-	}
-	log.Println("Finished decrypting private key using passphrase")
-
-	dec, err  := armor.Decode(strings.NewReader(message))
-	if err != nil {
-		return "decode message"
-	}
-
-	md, err := openpgp.ReadMessage(dec.Body, entityList, nil, nil)
-	if err != nil {
-		return "error read message"
-	}
-	output, err := ioutil.ReadAll(md.UnverifiedBody)
-	if err != nil {
-		return "error io read"
-	}
-	decStr := string(output)
-
-	return decStr
-}
-
-func (o *OpenPGP) Sample(message string) string {
-
-	return fmt.Sprintf("%s-end",message)
 }
