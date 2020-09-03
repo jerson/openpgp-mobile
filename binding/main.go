803ede9db6d4c7f833d527c1938ba9d4982d18dd
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2020-09-02 23:14:19 -0500

    added all structs for binding

--- binding/main.go
+++ binding/main.go
@@ -6,6 +6,8 @@ package main
 //typedef struct { char *hash; char *cipher; char *compression; char *compressionLevel; char *rsaBits; } KeyOptions;
 //typedef struct { char *name; char *comment; char *email; char *passphrase; KeyOptions *keyOptions; } Options;
 //typedef struct  { KeyPair* keyPair; char* error; } KeyPairReturn;
+//typedef struct  { void* message; int size; char* error; } SliceReturn;
+//typedef struct  { char* result; char* error; } StringReturn;
 import "C"
 import (
 	"github.com/jerson/openpgp-mobile/openpgp"
@@ -15,130 +17,225 @@ import (
 
 var instance = openpgp.NewFastOpenPGP()
 
-
 //export Encrypt
-func Encrypt(message, publicKey *C.char) *C.char {
+func Encrypt(message, publicKey *C.char) *C.StringReturn {
+	output := (*C.StringReturn)(C.malloc(C.size_t(C.sizeof_StringReturn)))
+	defer C.free(unsafe.Pointer(publicKey))
+	defer C.free(unsafe.Pointer(message))
+
 	result, err := instance.Encrypt(C.GoString(message), C.GoString(publicKey))
 	if err != nil {
-		return nil
+		output.error = C.CString(err.Error())
+		return output
 	}
-	return C.CString(result)
+	output.result = C.CString(result)
+	return output
 }
 
 //export EncryptBytes
-func EncryptBytes(message unsafe.Pointer, messageSize C.int, publicKey *C.char) (unsafe.Pointer, C.int) {
+func EncryptBytes(message unsafe.Pointer, messageSize C.int, publicKey *C.char) *C.SliceReturn {
+	output := (*C.SliceReturn)(C.malloc(C.size_t(C.sizeof_SliceReturn)))
+	defer C.free(unsafe.Pointer(publicKey))
+	defer C.free(message)
+
 	result, err := instance.EncryptBytes(C.GoBytes(message, messageSize), C.GoString(publicKey))
 	if err != nil {
-		return nil, C.int(0)
+		output.error = C.CString(err.Error())
+		return output
 	}
-	return C.CBytes(result), C.int(len(result))
+	output.message = C.CBytes(result)
+	output.size = C.int(len(result))
+	return output
 }
 
 //export Decrypt
-func Decrypt(message, privateKey, passphrase *C.char) *C.char {
+func Decrypt(message, privateKey, passphrase *C.char) *C.StringReturn {
+	output := (*C.StringReturn)(C.malloc(C.size_t(C.sizeof_StringReturn)))
+	defer C.free(unsafe.Pointer(passphrase))
+	defer C.free(unsafe.Pointer(message))
+	defer C.free(unsafe.Pointer(privateKey))
+
 	result, err := instance.Decrypt(C.GoString(message), C.GoString(privateKey), C.GoString(passphrase))
 	if err != nil {
-		return nil
+		output.error = C.CString(err.Error())
+		return output
 	}
-	return C.CString(result)
+	output.result = C.CString(result)
+	return output
 }
 
 //export DecryptBytes
-func DecryptBytes(message unsafe.Pointer, messageSize C.int, privateKey, passphrase *C.char) (unsafe.Pointer, C.int) {
+func DecryptBytes(message unsafe.Pointer, messageSize C.int, privateKey, passphrase *C.char) *C.SliceReturn {
+	output := (*C.SliceReturn)(C.malloc(C.size_t(C.sizeof_SliceReturn)))
+	defer C.free(unsafe.Pointer(passphrase))
+	defer C.free(unsafe.Pointer(privateKey))
+	defer C.free(message)
+
 	result, err := instance.DecryptBytes(C.GoBytes(message, messageSize), C.GoString(privateKey), C.GoString(passphrase))
 	if err != nil {
-		return nil, C.int(0)
+		output.error = C.CString(err.Error())
+		return output
 	}
-	return C.CBytes(result), C.int(len(result))
+	output.message = C.CBytes(result)
+	output.size = C.int(len(result))
+	return output
 }
 
 //export Sign
-func Sign(message, publicKey, privateKey, passphrase *C.char) *C.char {
+func Sign(message, publicKey, privateKey, passphrase *C.char) *C.StringReturn {
+	output := (*C.StringReturn)(C.malloc(C.size_t(C.sizeof_StringReturn)))
+	defer C.free(unsafe.Pointer(passphrase))
+	defer C.free(unsafe.Pointer(privateKey))
+	defer C.free(unsafe.Pointer(publicKey))
+	defer C.free(unsafe.Pointer(message))
+
 	result, err := instance.Sign(C.GoString(message), C.GoString(publicKey), C.GoString(privateKey), C.GoString(passphrase))
 	if err != nil {
-		return nil
+		output.error = C.CString(err.Error())
+		return output
 	}
-	return C.CString(result)
+	output.result = C.CString(result)
+	return output
 }
 
 //export SignBytes
-func SignBytes(message unsafe.Pointer, messageSize C.int, publicKey, privateKey, passphrase *C.char) (unsafe.Pointer, C.int) {
+func SignBytes(message unsafe.Pointer, messageSize C.int, publicKey, privateKey, passphrase *C.char) *C.SliceReturn {
+	output := (*C.SliceReturn)(C.malloc(C.size_t(C.sizeof_SliceReturn)))
+	defer C.free(unsafe.Pointer(passphrase))
+	defer C.free(unsafe.Pointer(privateKey))
+	defer C.free(message)
+
 	result, err := instance.SignBytes(C.GoBytes(message, messageSize), C.GoString(publicKey), C.GoString(privateKey), C.GoString(passphrase))
 	if err != nil {
-		return nil, C.int(0)
+		output.error = C.CString(err.Error())
+		return output
 	}
-	return C.CBytes(result), C.int(len(result))
+	output.message = C.CBytes(result)
+	output.size = C.int(len(result))
+	return output
 }
 
 //export SignBytesToString
-func SignBytesToString(message unsafe.Pointer, messageSize C.int, publicKey, privateKey, passphrase *C.char) *C.char {
+func SignBytesToString(message unsafe.Pointer, messageSize C.int, publicKey, privateKey, passphrase *C.char) *C.StringReturn {
+	output := (*C.StringReturn)(C.malloc(C.size_t(C.sizeof_StringReturn)))
+	defer C.free(unsafe.Pointer(passphrase))
+	defer C.free(unsafe.Pointer(privateKey))
+	defer C.free(unsafe.Pointer(publicKey))
+	defer C.free(message)
+
 	result, err := instance.SignBytesToString(C.GoBytes(message, messageSize), C.GoString(publicKey), C.GoString(privateKey), C.GoString(passphrase))
 	if err != nil {
-		return nil
+		output.error = C.CString(err.Error())
+		return output
 	}
-	return C.CString(result)
+	output.result = C.CString(result)
+	return output
 }
 
 //export Verify
-func Verify(signature, message, publicKey *C.char) *C.char {
+func Verify(signature, message, publicKey *C.char) *C.StringReturn {
+	output := (*C.StringReturn)(C.malloc(C.size_t(C.sizeof_StringReturn)))
+	defer C.free(unsafe.Pointer(publicKey))
+	defer C.free(unsafe.Pointer(message))
+	defer C.free(unsafe.Pointer(signature))
+
 	result, err := instance.Verify(C.GoString(signature), C.GoString(message), C.GoString(publicKey))
 	if err != nil {
-		return nil
+		output.error = C.CString(err.Error())
+		return output
 	}
 	if result {
-		return C.CString("1")
+		output.result = C.CString("1")
+	} else {
+		output.result = C.CString("")
 	}
-	return C.CString("")
+	return output
 }
 
 //export VerifyBytes
-func VerifyBytes(signature *C.char, message unsafe.Pointer, messageSize C.int, publicKey *C.char) *C.char {
+func VerifyBytes(signature *C.char, message unsafe.Pointer, messageSize C.int, publicKey *C.char) *C.StringReturn {
+	output := (*C.StringReturn)(C.malloc(C.size_t(C.sizeof_StringReturn)))
+	defer C.free(unsafe.Pointer(publicKey))
+	defer C.free(unsafe.Pointer(signature))
+	defer C.free(message)
+
 	result, err := instance.VerifyBytes(C.GoString(signature), C.GoBytes(message, messageSize), C.GoString(publicKey))
 	if err != nil {
-		return nil
+		output.error = C.CString(err.Error())
+		return output
 	}
 	if result {
-		return C.CString("1")
+		output.result = C.CString("1")
+	} else {
+		output.result = C.CString("")
 	}
-	return C.CString("")
+	return output
 }
 
 //export EncryptSymmetric
-func EncryptSymmetric(message, passphrase *C.char, options *C.KeyOptions) *C.char {
+func EncryptSymmetric(message, passphrase *C.char, options *C.KeyOptions) *C.StringReturn {
+	output := (*C.StringReturn)(C.malloc(C.size_t(C.sizeof_StringReturn)))
+	defer C.free(unsafe.Pointer(options))
+	defer C.free(unsafe.Pointer(passphrase))
+	defer C.free(unsafe.Pointer(message))
 
 	result, err := instance.EncryptSymmetric(C.GoString(message), C.GoString(passphrase), getKeyOptions(options))
 	if err != nil {
-		return nil
+		output.error = C.CString(err.Error())
+		return output
 	}
-	return C.CString(result)
+	output.result = C.CString(result)
+	return output
 }
 
 //export EncryptSymmetricBytes
-func EncryptSymmetricBytes(message unsafe.Pointer, messageSize C.int, passphrase *C.char, options *C.KeyOptions) (unsafe.Pointer, C.int) {
+func EncryptSymmetricBytes(message unsafe.Pointer, messageSize C.int, passphrase *C.char, options *C.KeyOptions) *C.SliceReturn {
+	output := (*C.SliceReturn)(C.malloc(C.size_t(C.sizeof_SliceReturn)))
+	defer C.free(unsafe.Pointer(options))
+	defer C.free(unsafe.Pointer(passphrase))
+	defer C.free(message)
 
 	result, err := instance.EncryptSymmetricBytes(C.GoBytes(message, messageSize), C.GoString(passphrase), getKeyOptions(options))
 	if err != nil {
-		return nil, C.int(0)
+		output.error = C.CString(err.Error())
+		return output
 	}
-	return C.CBytes(result), C.int(len(result))
+	output.message = C.CBytes(result)
+	output.size = C.int(len(result))
+	return output
 }
 
 //export DecryptSymmetric
-func DecryptSymmetric(message, passphrase *C.char, options *C.KeyOptions) *C.char {
+func DecryptSymmetric(message, passphrase *C.char, options *C.KeyOptions) *C.StringReturn {
+	output := (*C.StringReturn)(C.malloc(C.size_t(C.sizeof_StringReturn)))
+	defer C.free(unsafe.Pointer(options))
+	defer C.free(unsafe.Pointer(passphrase))
+	defer C.free(unsafe.Pointer(message))
+
 	result, err := instance.DecryptSymmetric(C.GoString(message), C.GoString(passphrase), getKeyOptions(options))
 	if err != nil {
-		return nil
+		output.error = C.CString(err.Error())
+		return output
 	}
-	return C.CString(result)
+	output.result = C.CString(result)
+	return output
 }
 
 //export DecryptSymmetricBytes
-func DecryptSymmetricBytes(message unsafe.Pointer, messageSize C.int, passphrase *C.char, options *C.KeyOptions) (unsafe.Pointer, C.int) {
+func DecryptSymmetricBytes(message unsafe.Pointer, messageSize C.int, passphrase *C.char, options *C.KeyOptions) *C.SliceReturn {
+	output := (*C.SliceReturn)(C.malloc(C.size_t(C.sizeof_SliceReturn)))
+	defer C.free(unsafe.Pointer(options))
+	defer C.free(unsafe.Pointer(passphrase))
+	defer C.free(message)
+
 	result, err := instance.DecryptSymmetricBytes(C.GoBytes(message, messageSize), C.GoString(passphrase), getKeyOptions(options))
 	if err != nil {
-		return nil, C.int(0)
+		output.error = C.CString(err.Error())
+		return output
 	}
-	return C.CBytes(result), C.int(len(result))
+	output.message = C.CBytes(result)
+	output.size = C.int(len(result))
+	return output
 }
 
 //export Generate
@@ -150,7 +247,7 @@ func Generate(optionsInput *C.Options) *C.KeyPairReturn {
 		output.error = C.CString(err.Error())
 		return output
 	}
-	output.keyPair = &C.KeyPair{C.CString(result.PublicKey),C.CString(result.PrivateKey)}
+	output.keyPair = &C.KeyPair{C.CString(result.PublicKey), C.CString(result.PrivateKey)}
 	return output
 
 }
