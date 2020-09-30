8accd3ca76758955bdeda5e5062d8d8e1c1e8abc
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2020-09-29 23:21:07 -0500

    deps

---
+++ openpgp/metadata_test.go
@@ -0,0 +1,32 @@
+package openpgp
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestFastOpenPGP_GetPublicKeyMetadata(t *testing.T) {
+
+	openPGP := NewFastOpenPGP()
+	output, err := openPGP.GetPublicKeyMetadata(publicKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	data, _ := json.MarshalIndent(output, "", " ")
+	t.Log(string(data))
+
+	t.Log("output:", output)
+}
+
+func TestFastOpenPGP_GetPrivateKeyMetadata(t *testing.T) {
+
+	openPGP := NewFastOpenPGP()
+	output, err := openPGP.GetPrivateKeyMetadata(privateKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	data, _ := json.MarshalIndent(output, "", " ")
+	t.Log(string(data))
+
+	t.Log("output:", output)
+}
