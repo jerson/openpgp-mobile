23022f2d95fa4f415906efc7033696b07b4a2d48
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2020-11-11 23:59:32 -0500

    fixed tti

--- cross_build.sh
+++ cross_build.sh
@@ -8,7 +8,7 @@ CMD=${CMD:-make build}
 ARGS=${ARGS:-}
 DOCKER_IMAGE_CROSS=docker.elastic.co/beats-dev/golang-crossbuild:${GOVERSION}
 
-docker run -it --rm -v "$PWD":/app -w /app \
+docker run -t --rm -v "$PWD":/app -w /app \
 	-e CGO_ENABLED=1 ${ARGS} \
 	"${DOCKER_IMAGE_CROSS}-${TAG}" \
 	--build-cmd "${CMD}" -p "${GOOS}/${GOARCH}"
