eb4a97bced4c17b5496f2404a1df4e2ca19e957f
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2022-07-04 22:51:39 -0500

    feat: Add convert and metadata methods

--- openpgp/metadata_test.go
+++ openpgp/metadata_test.go
@@ -18,6 +18,19 @@ func TestFastOpenPGP_GetPublicKeyMetadata(t *testing.T) {
 	t.Log("output:", output)
 }
 
+func TestFastOpenPGP_GetPublicKeyMetadataWithPrivateKeyShouldWork(t *testing.T) {
+
+	openPGP := NewFastOpenPGP()
+	output, err := openPGP.GetPublicKeyMetadata(privateKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	data, _ := json.MarshalIndent(output, "", " ")
+	t.Log(string(data))
+
+	t.Log("output:", output)
+}
+
 func TestFastOpenPGP_GetPrivateKeyMetadata(t *testing.T) {
 
 	openPGP := NewFastOpenPGP()
@@ -27,6 +40,17 @@ func TestFastOpenPGP_GetPrivateKeyMetadata(t *testing.T) {
 	}
 	data, _ := json.MarshalIndent(output, "", " ")
 	t.Log(string(data))
+
+	t.Log("output:", output)
+}
+
+func TestFastOpenPGP_GetPrivateKeyMetadataWithPublic(t *testing.T) {
+
+	openPGP := NewFastOpenPGP()
+	output, err := openPGP.GetPrivateKeyMetadata(publicKey)
+	if err == nil {
+		t.Fatal("must return error")
+	}
 
 	t.Log("output:", output)
 }
