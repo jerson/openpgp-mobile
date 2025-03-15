ba0e3220853e7400266d736f382c8366f9b92102
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2025-03-14 20:21:59 -0500

    add encoders y decoders for RN

--- binding/main.go
+++ binding/main.go
@@ -7,17 +7,43 @@ import "C"
 import (
 	"unsafe"
 
+	"github.com/jerson/helpers-mobile/codecs"
+
 	openPGPBridge "github.com/jerson/openpgp-mobile/bridge"
 )
 
 //export OpenPGPBridgeCall
 func OpenPGPBridgeCall(name *C.char, payload unsafe.Pointer, payloadSize C.int) *C.BytesReturn {
-	output := (*C.BytesReturn)(C.malloc(C.size_t(C.sizeof_BytesReturn)))
+	result, err := openPGPBridge.Call(C.GoString(name), C.GoBytes(payload, payloadSize))
+	return createBytesReturn(result, err)
+}
+
+//export OpenPGPEncodeText
+func OpenPGPEncodeText(input *C.char, encoding *C.char) *C.BytesReturn {
+	result, err := codecs.TextEncode(C.GoString(input), C.GoString(encoding))
+	return createBytesReturn(result, err)
+}
+
+//export OpenPGPDecodeText
+func OpenPGPDecodeText(input unsafe.Pointer, size C.int, encoding *C.char, fatal C.int, ignoreBOM C.int, stream C.int) *C.char {
+	inputBytes := C.GoBytes(input, size)
+	options := codecs.TextDecoderOptions{Fatal: fatal != 0, IgnoreBOM: ignoreBOM != 0}
+	decodeOptions := codecs.TextDecodeOptions{Stream: stream != 0}
+	result, err := codecs.TextDecode(inputBytes, C.GoString(encoding), options, decodeOptions)
+	if err != nil {
+		return C.CString("")
+	}
+	return C.CString(result)
+}
+
+func createBytesReturn(result []byte, err error) *C.BytesReturn {
+	output := (*C.BytesReturn)(C.malloc(C.size_t(unsafe.Sizeof(C.BytesReturn{}))))
 	// we should free resources on dart side
 
-	result, err := openPGPBridge.Call(C.GoString(name), C.GoBytes(payload, payloadSize))
 	if err != nil {
 		output.error = C.CString(err.Error())
+		output.message = nil
+		output.size = 0
 		return output
 	}
 	output.error = nil
--- go.mod
+++ go.mod
@@ -1,16 +1,16 @@
 module github.com/jerson/openpgp-mobile
 
-go 1.22.0
-
-toolchain go1.23.3
+go 1.24.1
 
 require (
 	github.com/ProtonMail/go-crypto v1.1.3
 	github.com/google/flatbuffers v24.3.25+incompatible
+	github.com/jerson/helpers-mobile v0.0.0-20250315011617-729f4804866f
 	golang.org/x/crypto v0.29.0
 )
 
 require (
 	github.com/cloudflare/circl v1.3.7 // indirect
 	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
 )
